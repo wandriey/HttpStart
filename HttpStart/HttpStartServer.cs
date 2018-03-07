af783cd0b03a5a01ac0694a5d8416fc3714d2e4f
wandriey <jona434g@edu.easj.dk>
2018-03-07 10:41:14 +0100

    Initial commit

---
+++ HttpStart/HttpStartServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpStart
+{
+    class HttpStartServer
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("172.20.10.2");   //
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+            serverSocket.Start();
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                //Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated");
+
+                Services services = new Services(connectionSocket);
+                // services.doIt();                                      //Vi skal ikke kalde doit metoden selv, da thread tager det som parameter
+
+
+                Thread thread = new Thread(services.doIt);              //Hver gang man løber en while loopen igennem, så laves der en ny tråd.
+                thread.Start();
+
+                // Task.Run(()=>services.doIt());          //ved at bruge task, har vi en "pool" så vi genbruger threads istedet for at smide dem væk efter brug
+            }
+            serverSocket.Stop();
+        }
+    }
+}
---
+++ HttpStart/Services.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpStart
+{
+    class Services
+    {
+        private TcpClient _connectionSocket;
+        public Services(TcpClient connection)
+        {
+            _connectionSocket = connection;
+        }
+
+        public void doIt()
+        {
+            Stream ns = _connectionSocket.GetStream();
+            //Stream ns = new NetworkStream(connectionSocket);
+            StreamReader sr = new StreamReader(ns); 
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+
+            do
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            } while (message != null && message != "STOP");
+            Console.WriteLine("Server Stopped");
+            
+            ns.Close();
+            _connectionSocket.Close();
+        }
+    }
+}
